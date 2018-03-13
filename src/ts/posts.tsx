481b8493bf536ec1fe6d320d8501332afe1dfba4
ksaanen <ksaanen@independer.nl>
2018-03-13 16:22:26 +0100

    make data connection

--- src/ts/posts.tsx
+++ src/ts/posts.tsx
@@ -1,41 +1,70 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import axios from 'axios';
 
 interface PostInterface {
-  id: Number;
-  author: Number; // TODO: Get author by ID
-  title: string;
-  content: string;
-  exerpt: string;
+  id: any;
+  author: any;
+  title: any;
+  content: any;
+  exerpt: any;
 }
 
 class Post extends React.Component<PostInterface> {
-  constructor(props) {
+
+  constructor(props:PostInterface) {
     super(props);
   }
-
+  
   render() {
     return (
-      <div data-class="wp-post">
-        <div>id: {this.props.id}</div>
-        <div>author: {this.props.author}</div>
-        <div>title: {this.props.title}</div>
-        <div>content: {this.props.content}</div>
-        <div>exerpt: {this.props.exerpt}</div>
+      <div className="wp-post">
+        {JSON.stringify(this.props)}
+        <div className="wp-post--id">{this.props.id}</div>
+        <div className="wp-post--author">{this.props.author}</div>
+        <div className="wp-post--title">{this.props.title}</div>
+        <div className="wp-post--content">{this.props.content}</div>
+        <div className="wp-post--exerpt">{this.props.exerpt}</div>
       </div>
     );
   }
+  
 }
+
  
 class Posts extends React.Component {
 
-  // TODO: Make loop trough posts with axios;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    let data_url = '//wordpress.ilovejs.nl/wp-json/wp/v2/posts';
+    fetch(data_url)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({posts: response});
+      });
+    }
 
   render() {
+
     return (
-      <h1>Posts</h1>
+      <div className="wp-posts">
+        <h1>Posts</h1>
+        {
+          // this.state.posts.map((post, index) => {
+          //   return <Post key="{index}" id="{post.id}" author="{post.author}" title="{post.title}" content="{post.content}" exerpt="{post.exerpt}" />
+          // })
+        }
+      </div>
     );
-  } 
+  }
+
 }
 
 export default Posts;
--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,12 @@ const config = {
   devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
-    contentBase: './wwwroot/'
+    contentBase: './wwwroot/',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   }
 };
 
