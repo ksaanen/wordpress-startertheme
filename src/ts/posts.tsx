76f5f978c6c24e2d2865a17b3dfe50cd3b12200f
ksaanen <ksaanen@independer.nl>
2018-03-13 16:46:53 +0100

    we got data!

--- src/ts/posts.tsx
+++ src/ts/posts.tsx
@@ -3,11 +3,21 @@ import * as ReactDOM from 'react-dom';
 import axios from 'axios';
 
 interface PostInterface {
-  id: any;
-  author: any;
-  title: any;
-  content: any;
-  exerpt: any;
+  id: Number;
+  author: Number;
+  title: PostTitleInterface;
+  content: PostContentInterface;
+  exerpt?: PostExerptInterface;
+}
+
+interface PostTitleInterface {
+  rendered: String
+}
+interface PostContentInterface {
+  rendered: String
+}
+interface PostExerptInterface {
+  rendered: String
 }
 
 class Post extends React.Component<PostInterface> {
@@ -18,13 +28,12 @@ class Post extends React.Component<PostInterface> {
   
   render() {
     return (
-      <div className="wp-post">
-        {JSON.stringify(this.props)}
+      <div key="{this.props.key}" className="wp-post">
         <div className="wp-post--id">{this.props.id}</div>
-        <div className="wp-post--author">{this.props.author}</div>
-        <div className="wp-post--title">{this.props.title}</div>
-        <div className="wp-post--content">{this.props.content}</div>
-        <div className="wp-post--exerpt">{this.props.exerpt}</div>
+        <div className="wp-post--author">written by: {this.props.author}</div>
+        <div className="wp-post--title">{this.props.title.rendered}</div>
+        <div className="wp-post--content">{this.props.content.rendered}</div>
+        <div className="wp-post--exerpt">{this.props.exerpt.rendered}</div>
       </div>
     );
   }
@@ -57,9 +66,9 @@ class Posts extends React.Component {
       <div className="wp-posts">
         <h1>Posts</h1>
         {
-          // this.state.posts.map((post, index) => {
-          //   return <Post key="{index}" id="{post.id}" author="{post.author}" title="{post.title}" content="{post.content}" exerpt="{post.exerpt}" />
-          // })
+          this.state.posts.map((post, index) => {
+            return <Post key={index} id={post.id} author={post.author} title={post.title} content={post.content} exerpt={post.exerpt || ''} />
+          })
         }
       </div>
     );
