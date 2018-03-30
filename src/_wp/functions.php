641edaf369e98f8718af115121d1914f968df9c2
ksaanen <ksaanen@independer.nl>
2018-03-30 15:37:05 +0200

    add REST API menu

--- src/_wp/functions.php
+++ src/_wp/functions.php
@@ -1,13 +1,23 @@
 <?php
     
-    add_action('init', 'custom_rewrite_rule');
-    
-    // Add rewrite rule to make use of index.html file instead of default php file
-    function custom_rewrite_rule() {
-      $regex = '/';
-      $location = 'index.php';
-      $priority = 'top';
-      add_rewrite_rule( $regex, $location, $priority );
+    // Activate menu support
+    function register_my_menu() {
+      register_nav_menu('header-menu',__( 'Header Menu' ));
     }
+    add_action( 'init', 'register_my_menu' );
+
+
+    // Register custom REST API menu
+    function get_menu() {
+      # Change 'menu' to your own navigation slug.
+      return wp_get_nav_menu_items('header-menu');
+    }
+  
+    add_action('rest_api_init', function(){
+        register_rest_route( 'header', '/menu', array(
+        'methods' => 'GET',
+        'callback' => 'get_menu',
+      ));
+    });
 
 ?>
--- src/ts/components/menu.tsx
+++ src/ts/components/menu.tsx
@@ -2,16 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link } from 'react-router-dom';
 
-class MainMenu extends React.Component {
+interface MenuItemInterface {
+  title: String;
+  url: String;
+  post_name: String;
+}
 
-  render(){
+interface MainMenuInterfaceProps {
+  items?: Array<MenuItemInterface>;
+}
+
+interface MainMenuInterface {
+  items?: Array<MenuItemInterface>;
+}
+
+class MainMenu extends React.Component<MainMenuInterfaceProps, MainMenuInterface> {
+
+  constructor(props: MainMenuInterfaceProps) {
+    super(props);
+
+    this.state = {
+      items: this.props.items
+    };
+  }
+
+  filterURL(url, filter) {
+    return url.replace("filter", '');
+  }
+
+  componentDidMount() {
+    let data_url = '//wordpress.ilovejs.nl/wp-json/header/menu';
+    fetch(data_url)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({items: response});
+      });
+    }
+  
+  render() {
+    if (!this.state.items) {
+      return <ul></ul>
+    }
     return (
       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/posts">Posts</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/pages">Pages</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+        {
+          this.state.items.map((item, index) => {
+            return (
+              <li key={index} className="nav-item">
+                <Link className="nav-link" to="{${item.title}}">{item.title}</Link>
+              </li>
+            );
+          })
+        }
       </ul>
     );
   }
